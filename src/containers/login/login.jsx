0d80a3f54ef4307645196990420af0e3c75a7ef7
ZhangYW18 <yiweizh18@gmail.com>
2022-06-03 20:29:03 +0800

    add static login page

--- src/containers/login/login.jsx
+++ src/containers/login/login.jsx
@@ -2,16 +2,72 @@
   Login Component
  */
 
-import React, {Component} from 'react';
+import React from 'react';
+import {
+  Button, NavBar, Input, Form, Space
+} from "antd-mobile";
+import Logo from "../../components/logo/logo";
+import {useState} from "react";
+import {useNavigate} from "react-router-dom";
 
-class Login extends Component {
-  render() {
-    return (
-      <div>
-        Login
-      </div>
-    );
+export default () => {
+
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  let navigate = useNavigate();
+  const toRegister = () => navigate("/register", { replace: true })
+
+  const login = () => {
+    console.log(username)
+    console.log(password)
   }
-}
 
-export default Login;
+  return (
+    <div>
+      <Space direction='vertical'>
+
+        <NavBar backArrow={false}>
+          JobFinder
+        </NavBar>
+
+        <Logo/>
+
+        <Form
+          layout='horizontal'
+          footer={
+            <Button block onClick={login} color='primary' size='middle'>
+              Login
+            </Button>
+          }
+        >
+          <Form.Item label='Username' name='username'>
+            <Input
+              placeholder='Username'
+              value={username}
+              onChange={val => {
+                setUsername(val)
+              }}
+              clearable />
+          </Form.Item>
+
+          <Form.Item label='Password' name='password'>
+            <Input
+              placeholder='Password'
+              type='password'
+              value={password}
+              onChange={val => {
+                setPassword(val)
+              }}
+              clearable />
+          </Form.Item>
+        </Form>
+
+        <Button block onClick={toRegister} size='middle'>
+          Register
+        </Button>
+
+      </Space>
+    </div>
+  );
+}
